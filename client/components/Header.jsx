644792bc11ab85e1a09d8db8bea045a1558a6c00
Brianna Sookhoo <briannasookhoo@Briannas-MacBook-Air.local>
2020-04-23 11:32:50 -0700

    master branch back to clean

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,15 +1,12 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { updateData } from '../redux/actions/actions';
 import { setUserAuth } from '../redux/actions/actions';
 import { Link } from 'react-router-dom';
 import Button from './Button';
 import '../styles/Header.scss';
 
 const Header = ({userID}) => {
-  const user = useSelector((state) => state.user);
   const dispatch = useDispatch()
-  const [updateCash, setUpdateCash] = useState(0);
   
   const handleLogOut = () => {
     return dispatch(setUserAuth({
@@ -19,26 +16,6 @@ const Header = ({userID}) => {
       stocks: [],
     }));
   }
-
-  const handleCashChange = (e) => {
-    const value = e.target.value;
-    setUpdateCash(value);
-  };
-
-  const handleCashClick = () => {
-    fetch(`/api/cash`, { // need to write backend route
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        _id: user._id,
-        cash: updateCash,
-      }),
-    })
-    .then((res) => res.json())
-    .then((data) => dispatch(updateData(data)))
-    .catch((err) => console.log('ERROR while updating cash ', err));
-  }
-
 
  console.log('userID: ', userID)
   if (!userID) {
@@ -76,10 +53,6 @@ const Header = ({userID}) => {
       </div>
     </Link>
       <div className='options'>
-      <input type="text" onChange={handleCashChange} />
-        <Button secondary mr1 onClick={handleCashClick}>
-            Update Cash
-        </Button>
         <Link to='/app/login'>
           <Button secondary mr1 onClick={handleLogOut}>
             Logout
